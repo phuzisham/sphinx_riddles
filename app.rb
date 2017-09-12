da41a7388f3812682987fea6fb1abbf9890d9327
David Hale and Javier Jimenez <david@>
2017-09-12 16:14:50 -0700

    add multiple possible answers

--- app.rb
+++ app.rb
@@ -6,15 +6,17 @@ require('./lib/sphinx')
 new_riddle = Sphinx.new
 get('/') do
   # new_riddle = Sphinx.new
-  @display = new_riddle.question
+  @display = new_riddle.riddle_picker
   erb(:input)
 end
 
 post('/output') do
   if new_riddle.riddle_test?(params.fetch('answer'))
     @result = 'Right Answer'
+    erb(:output)
   else
-    @result = 'Wrong Answer'
+    @display = new_riddle.question
+    @try = 'Wrong Answer. Try Again.'
+    erb(:fail)
   end
-  erb(:output)
 end
--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -2,16 +2,17 @@ class Sphinx
   attr_reader  :question, :riddles
   def initialize()
     @riddles = {
-      'riddle1' => 'secret',
-      'riddle2' => 'unknown',
-      'riddle3' => 'who knows'}
-    @question = riddles.keys.sample()
+      'If you have it you want to share it but if you share it you haven\'t got it.' => ['secret', 'a secret'],
+      'riddle2' => ['unknown', 'not known'],
+      'riddle3' => ['who knows', 'nobody knows']}
+  end
+
+  def riddle_picker
+    @question = @riddles.keys.sample()
   end
 
   def riddle_test?(answer)
-    if (@riddles.fetch(@question) == answer)
-      true
-    end
+    @riddles.fetch(@question).include? answer
   end
 
 end
